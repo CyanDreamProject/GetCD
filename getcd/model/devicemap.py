59ba17f5dc01d5d09969624459dc468526d13952
yanniks <kontakt@yanniks.de>
2013-09-15 12:10:39 +0200

    added other m7 variants

--- getcd/model/devicemap.py
+++ getcd/model/devicemap.py
@@ -1,6 +1,9 @@
 devicemap = {
     'ace': 'HTC Desire HD',
     'bravo': 'HTC Desire',
+    'm7att': 'HTC One (AT&T)',
+    'm7spr': 'HTC One (Sprint)',
+    'm7tmo': 'HTC One (T-Mobile)',
     'm7ul': 'HTC One (GSM)',
     'maguro': 'Samsung Galaxy Nexus',
     'mako': 'Google Nexus 4',
