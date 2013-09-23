28d268638f5cfa2204f3e62e84bf2654b4a331fd
yanniks <kontakt@yanniks.de>
2013-09-23 15:55:56 +0200

    added t0lte and crespo devices

--- getcd/model/devicemap.py
+++ getcd/model/devicemap.py
@@ -1,11 +1,13 @@
 devicemap = {
     'ace': 'HTC Desire HD',
     'bravo': 'HTC Desire',
+    'crespo': 'Google Nexus S',
     'm7att': 'HTC One (AT&T)',
     'm7spr': 'HTC One (Sprint)',
     'm7tmo': 'HTC One (T-Mobile)',
     'm7ul': 'HTC One (GSM)',
     'maguro': 'Samsung Galaxy Nexus',
     'mako': 'Google Nexus 4',
-    'n7100': 'Samsung Galaxy Note II'
+    'n7100': 'Samsung Galaxy Note II',
+    't0lte': 'Samsung Galaxy Note II (GSM LTE)'
 }
