29e78809777d7a3bb400f16547d003b19da6b56e
cyandream-gerrit <kalender2@yanniks.de>
2013-11-23 17:35:38 +0100

    add m7vzw to devicemap

--- getcd/model/devicemap.py
+++ getcd/model/devicemap.py
@@ -7,6 +7,7 @@ devicemap = {
     'm7spr': 'HTC One (Sprint)',
     'm7tmo': 'HTC One (T-Mobile)',
     'm7ul': 'HTC One (GSM)',
+    'm7vzw': 'HTC One (Verizon)',
     'maguro': 'Samsung Galaxy Nexus',
     'mako': 'Google Nexus 4',
     'n7100': 'Samsung Galaxy Note II',
