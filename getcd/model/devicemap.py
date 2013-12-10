edec07adc4469f3c7e6a5597bcd73f089b308aa0
yanniks <kontakt@yanniks.de>
2013-12-10 14:12:46 +0100

    add p3110 and galaxysmtd to devicemap
    Change-Id: Icdb8597c55df208239cb08ad165a8516b38bd276

--- getcd/model/devicemap.py
+++ getcd/model/devicemap.py
@@ -2,6 +2,7 @@ devicemap = {
     'ace': 'HTC Desire HD',
     'bravo': 'HTC Desire',
     'crespo': 'Google Nexus S',
+    'galaxysmtd': 'Samsung Galaxy S',
     'm7att': 'HTC One (AT&T)',
     'm7spr': 'HTC One (Sprint)',
     'm7tmo': 'HTC One (T-Mobile)',
@@ -9,5 +10,6 @@ devicemap = {
     'maguro': 'Samsung Galaxy Nexus',
     'mako': 'Google Nexus 4',
     'n7100': 'Samsung Galaxy Note II',
+    'p3110': 'Samsung Galaxy Tab 2 7.0 (Wi-Fi)',
     't0lte': 'Samsung Galaxy Note II (GSM LTE)'
 }
